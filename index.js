7af6fe3360e99102008dc6430b0f47552c625782
Md. Tahsin Amin <tahsin16@cse.pstu.ac.bd>
2023-01-28 14:02:24 +0600

    temperature increase

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post("/message", async (req, res) => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${message}`,
-      temperature: 0,
+      temperature: 1,
       max_tokens: 3000,
       top_p: 1,
       frequency_penalty: 0.5,
