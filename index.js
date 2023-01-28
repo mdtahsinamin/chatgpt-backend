cab5a4dc65f6680559ba5531d1e41fc8dd35dc92
Md. Tahsin Amin <tahsin16@cse.pstu.ac.bd>
2023-01-28 18:16:45 +0600

    cors update

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ require("dotenv").config();
 const { Configuration, OpenAIApi } = require("openai");
 const app = express();
 
+var corsOptions = {
+  origin: "http://localhost:3000/" || "https://chatgptv2-9d9a1.web.app/",
+};
+
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const configuration = new Configuration({
   apiKey: process.env.API_KEY,
@@ -24,11 +28,8 @@ app.post("/message", async (req, res) => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${message}`,
-      temperature: 1,
-      max_tokens: 3000,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0,
+      max_tokens: 100,
+      temperature: 0.5,
     });
     res.status(200).send({
       message: response.data.choices[0].text,
@@ -44,3 +45,14 @@ const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+/*
+model: "text-davinci-003",
+      prompt: `${message}`,
+      temperature: 1,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+
+*/
